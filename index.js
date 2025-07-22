8490009fcc32927e02f7bb44772153099b590abe
Adarsh Sh <153447837+ADARSH07SH@users.noreply.github.com>
2025-07-22 21:32:17 +0530

    initial code setup

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+require("dotenv").config();
+const { fyersModel } = require("fyers-api-v3");
+
+const app = express();
+const PORT = 8080;
+
+// Load env variables
+const FYERS_APP_ID = process.env.FYERS_APP_ID;
+const FYERS_SECRET_ID = process.env.FYERS_SECRET_ID;
+const FYERS_REDIRECT_URL = process.env.FYERS_REDIRECT_URL;
+
+
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+const fyers = new fyersModel({
+  path: "./logs",
+  enableLogging: true,
+});
+
+fyers.setAppId(FYERS_APP_ID);
+fyers.setRedirectUrl(FYERS_REDIRECT_URL);
+
+
+app.get("/login", (req, res) => {
+  try {
+    const authUrl = fyers.generateAuthCode();
+    console.log("Generated Auth URL:", authUrl);
+    res.redirect(authUrl);
+  } catch (err) {
+    console.error("Error generating auth URL:", err);
+    res.status(500).send("Failed to generate auth URL.");
+  }
+});
+
+
+app.get("/admin", async (req, res) => {
+  const auth_code = req.query.auth_code;
+  if (!auth_code) return res.status(400).send("Missing auth_code from Fyers.");
+
+  try {
+    const tokenResponse = await fyers.generate_access_token({
+      client_id: FYERS_APP_ID,
+      secret_key: FYERS_SECRET_ID,
+      auth_code: auth_code,
+    });
+
+    console.log("Access Token:", tokenResponse);
+    res.send("Access token generated. Check console.");
+  } catch (err) {
+    console.error("Error generating access token:", err);
+    res.status(500).send("Token generation failed.");
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
