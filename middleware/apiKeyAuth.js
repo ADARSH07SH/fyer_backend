fc12b0f0b9fdf5312ad3ae95086dbf1d2da2aab1
Adarsh Sh <153447837+ADARSH07SH@users.noreply.github.com>
2025-08-27 16:07:19 +0530

    4.1

--- middleware/apiKeyAuth.js
+++ middleware/apiKeyAuth.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
   const apiKey = req.headers["x-api-key"];
-  console.log("Incoming API Key:", apiKey);
+  
 
   if (!apiKey || apiKey !== process.env.FYER_API_KEY) {
     console.log("Unauthorized request. Invalid or missing API key.");
